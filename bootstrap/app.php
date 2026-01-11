003ae2d31f1d7d17f5116e792b5dc8c5d664ffe9
manishachar22-ops <manishachar22@gmail.com>
2026-01-11 12:30:57 +0530

    10

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
 
         $middleware->appendToGroup('api', \Illuminate\Http\Middleware\HandleCors::class);
         
